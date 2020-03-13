cb9b287056b36dba893c4193266f6771a397c8e0
Lisa Baranovskaya <liza.baranovvvskaya@gmail.com>
2020-03-13 15:06:44 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (arguments.length === 0 || array.length === 0) {
+    return 0;
+  }
+  return Math.min.apply(Math, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (arguments.length === 0 || array.length === 0) {
+    return 0;
+  }
+  return Math.max.apply(Math, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (arguments.length === 0 || array.length === 0) {
+    return 0;
+  }
+  let sum = array.reduce((a, b) => a + b, 0);
+  return sum / array.length;
 }
